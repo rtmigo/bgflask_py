540ee826a80e18f630594532e397f4b6ad34c8e7
rtmigo <art.galkin@gmail.com>
2021-04-30 03:48:29 +0300

    importbug

--- flaskrun/the.py
+++ flaskrun/the.py
@@ -23,7 +23,7 @@ class FlaskRunner:
 
         self.server = BackgroundProcess(cmd, buffer_output=True,
                                         #add_env=self.add_env,
-                                        add_env={"PYTHONPATH": os.path.abspath('.')+":"+os.environ.get("PYTHONPATH")},
+                                        add_env={"PYTHONPATH": os.path.abspath('.')+":"+(os.environ.get("PYTHONPATH") or '')},
                                         #cwd=os.path.abspath('.')
                                         )
         self.server.start()
